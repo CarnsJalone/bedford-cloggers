90bbc5acf7542c9dd0c986b1a3b4588b2a480d2e
Jarret Laberdee <jarret.laberdee@basf.com>
2023-12-02 19:11:00 -0500

    stuff

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,16 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
+        pathMatch: 'full',
         loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
     },
     {
         path: 'calendar',
+        pathMatch: 'full',
         loadChildren: () => import('./calendar/calendar.module').then(m => m.CalendarModule)
+    },
+    {
+        path: '**',
+        redirectTo: ''
     }
 ];
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,18 +1,36 @@
-import { Component } from "@angular/core";
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import { RouterModule } from "@angular/router";
+import { Component, inject } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AsyncPipe } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { RouterModule } from '@angular/router';
 
 @Component({
-    templateUrl: './navigation.component.html',
-    styleUrls: ['./navigation.component.scss'],
-    selector: 'navigation',
-    standalone: true,
-    imports: [
-        MatToolbarModule,
-        MatIconModule,
-        RouterModule
-    ]
+  selector: 'navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss'],
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    RouterModule,
+    AsyncPipe,
+  ]
 })
 export class NavigationComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 }
